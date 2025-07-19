39519652b048cffe388e8897fb61ce200f4c4256
Mike Attara <mikeattara.gh@gmail.com>
2025-07-19 14:46:43 +0000

    update serializer

--- alx_travel_app/listings/serializers.py
+++ alx_travel_app/listings/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 from .models import Listing, Booking, Review
-from django.contrib.auth.models import User
 
 
 class ReviewSerializer(serializers.ModelSerializer):
